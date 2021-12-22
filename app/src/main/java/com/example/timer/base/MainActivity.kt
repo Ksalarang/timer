a7ff5e3c4d4ca5366501bd065bbe781324da5464
ksalarang <anbleidd@gmail.com>
2021-12-22 06:23:25 +0600

    Revert "Change app theme"
    This reverts commit cad3563fa8dc3de61fc13f165a5d286ac8c89521.

--- app/src/main/java/com/example/timer/base/MainActivity.kt
+++ app/src/main/java/com/example/timer/base/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
