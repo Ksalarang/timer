2c0b405443de72376bb1485bff8a5b7550f44bba
ksalarang <anbleidd@gmail.com>
2021-12-29 01:50:01 +0600

    Add method - isNougatOrAbove

--- app/src/main/java/com/example/timer/utils/Utils.kt
+++ app/src/main/java/com/example/timer/utils/Utils.kt
@@ -9,6 +9,7 @@ import android.view.inputmethod.InputMethodManager
 class Utils {
     companion object {
         fun isOreoOrAbove() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
+        fun isNougatOrAbove() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
         fun isMarshmallowOrAbove() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
 
         fun hideKeyboard(context: Context, view: View) {
