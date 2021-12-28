4f55396fb833fd49d09c4e4fddf5a46772b90298
ksalarang <anbleidd@gmail.com>
2021-12-29 04:05:40 +0600

    Add 3 companion object methods to LocaleHelper:
    - setLocale, updateResources, updateResourcesLegacy

--- app/src/main/java/com/example/timer/base/LocaleHelper.kt
+++ app/src/main/java/com/example/timer/base/LocaleHelper.kt
@@ -1,4 +1,48 @@
 package com.example.timer.base
 
+import android.annotation.TargetApi
+import android.content.Context
+import android.os.Build
+import com.example.timer.utils.Utils
+import java.util.*
+
+private const val PREF_SELECTED_LANGUAGE = "selectedLanguage"
+
 class LocaleHelper {
+    companion object {
+        fun setLocale(context: Context, language: String): Context {
+            return if (Utils.isNougatOrAbove()) {
+                updateResources(context, language)
+            } else {
+                updateResourcesLegacy(context, language)
+            }
+        }
+
+        @TargetApi(Build.VERSION_CODES.N)
+        private fun updateResources(context: Context, language: String): Context {
+            val locale = Locale(language)
+            Locale.setDefault(locale)
+
+            val configuration = context.resources.configuration.also {
+                it.setLocale(locale)
+                it.setLayoutDirection(locale)
+            }
+            return context.createConfigurationContext(configuration)
+        }
+
+        @Suppress(SUPPRESS_DEPRECATION)
+        private fun updateResourcesLegacy(context: Context, language: String): Context {
+            val resources = context.resources
+            val locale = Locale(language)
+            Locale.setDefault(locale)
+
+            val configuration = resources.configuration.also {
+                it.locale = locale
+                it.setLayoutDirection(locale)
+            }
+            resources.updateConfiguration(configuration, resources.displayMetrics)
+
+            return context
+        }
+    }
 }
