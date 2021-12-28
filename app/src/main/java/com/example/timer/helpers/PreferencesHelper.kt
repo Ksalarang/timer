88f354b17048d0d6da343038b3c7fee038c905f6
ksalarang <anbleidd@gmail.com>
2021-12-29 04:25:59 +0600

    Add class to helpers package - PreferencesHelper:
    - Add 4 methods: getIntPreference, getStringPreference, saveIntPreference, saveStringPreference

---
+++ app/src/main/java/com/example/timer/helpers/PreferencesHelper.kt
@@ -0,0 +1,29 @@
+package com.example.timer.helpers
+
+import androidx.appcompat.app.AppCompatActivity
+
+class PreferencesHelper {
+    companion object {
+        fun getIntPreference(activity: AppCompatActivity, key: String, defaultValue: Int = 0): Int {
+            return activity.getPreferences(AppCompatActivity.MODE_PRIVATE).getInt(key, defaultValue)
+        }
+
+        fun getStringPreference(activity: AppCompatActivity, key: String, defaultValue: String? = null): String? {
+            return activity.getPreferences(AppCompatActivity.MODE_PRIVATE).getString(key, defaultValue)
+        }
+
+        fun saveIntPreference(activity: AppCompatActivity, key: String, value: Int) {
+            activity.getPreferences(AppCompatActivity.MODE_PRIVATE)
+                .edit()
+                .putInt(key, value)
+                .apply()
+        }
+
+        fun saveStringPreference(activity: AppCompatActivity, key: String, value: String) {
+            activity.getPreferences(AppCompatActivity.MODE_PRIVATE)
+                .edit()
+                .putString(key, value)
+                .apply()
+        }
+    }
+}
